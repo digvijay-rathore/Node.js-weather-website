66a2edee131d964a46d230fa404340e32b9e3623
Digvijay Rathore <digvijaysjodha@gmail.com>
2020-07-03 05:09:21 +0530

    Database changed to 'weather'

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-const MongoDb_URL = 'mongodb+srv://digvijay-weather-application:Madhu54321@cluster0.q94mr.mongodb.net/test?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/test'
+const MongoDb_URL = 'mongodb+srv://digvijay-weather-application:Madhu54321@cluster0.q94mr.mongodb.net/weather?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/test'
 
 mongoose.connect(MongoDb_URL, { useNewUrlParser : true, useCreateIndex : true, useUnifiedTopology : true});
